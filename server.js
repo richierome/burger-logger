57faf2f6f246b56089c81ea255b665d4b2858088
richie <rickromero99@gmail.com>
2019-05-04 11:43:09 -0700

    updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("handlebars");
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
